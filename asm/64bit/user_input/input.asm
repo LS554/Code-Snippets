6efe891e51b02d708764297b6ff63f30c3bf306c
LS554 <103077889+LS554@users.noreply.github.com>
2025-06-09 18:42:00 -0400

    Add asm_64_input and update "_hello readme

---
+++ asm/64bit/user_input/input.asm
@@ -0,0 +1,58 @@
+global _start
+
+section .data
+  welcome_message: db "Please enter a message: ", 10 ; define welcome message
+  wmLen: equ $-welcome_message ; length of welcome
+  goodbye_message: db "You entered: ", 10 ; define goodbye message
+  gmLen: equ $-goodbye_message ; length of welcome
+
+section .bss
+  buffer resb 256
+
+section .text
+  _start:
+  ;start welcome 
+  mov rax, 1 ; syswrite
+  mov rdi, 1 ; stdout
+  mov rsi, welcome_message; write message 
+  mov rdx, wmLen ; length of message
+  syscall
+  ;end welcome 
+
+  ;start read
+  mov rax, 0 ; sysread
+  mov rdi, 0 ; stdin
+  mov rsi, buffer ; save to buffer
+  mov rdx, 256
+  syscall
+  ;end read 
+
+  ;start goodbye 
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, goodbye_message
+  mov rdx, gmLen
+  syscall
+  ;end read
+
+  ;start print input
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, buffer
+  mov rdx, 256
+  syscall
+  ;end print input 
+
+  ;start exit
+  mov rax, 60 ; sysexit
+  mov rdi, 0 ; exit code 0
+  syscall
+  ;end exit
+
+
+
+
+
+
+
+
